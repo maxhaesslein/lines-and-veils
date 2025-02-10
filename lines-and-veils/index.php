9b14fa0c5f008ff3d7ebb5ed7e4dc60b112060c0
maxhaesslein <git@maxhaesslein.de>
2025-02-10 12:21:18 +0100

    init

---
+++ lines-and-veils/index.php
@@ -0,0 +1,16 @@
+<?php
+
+define( 'TTRPG-LV', true );
+
+$version = '0.1';
+
+include_once('snippets/head.php');
+?>
+<body id="offline">
+
+<main>
+	<h1>Lines & Veils</h1>
+</main>
+
+</body>
+</html>
---
+++ lines-and-veils/snippets/head.php
@@ -0,0 +1,29 @@
+<?php
+
+if( ! defined('TTRPG-LV') ) exit;
+
+?><!DOCTYPE html>
+<html lang="de-DE">
+<head>
+	<meta charset="utf-8">
+
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<meta name="viewport" content="viewport-fit=cover, user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>Lines & Veils</title>
+
+	<meta name="version" content="<?= $version ?>">
+
+	<?php /* ?>
+	<link rel="icon" type="image/png" href="<?= $favicon_path ?>favicon-96x96.png" sizes="96x96" />
+	<link rel="shortcut icon" href="<?= $favicon_path ?>favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= $favicon_path ?>apple-touch-icon.png" />
+	</php */ ?>
+
+	<?php /* <link rel="stylesheet" href="assets/css/fonts.css?v1" type="text/css" media="all"> */ ?>
+	<link rel="stylesheet" href="assets/css/style.css?v=<?= $version ?>" type="text/css" media="all">
+
+	<script type="text/javascript" src="assets/js/main.js?v=<?= $version ?>"></script>
+
+</head>
