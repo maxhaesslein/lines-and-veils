20aac64b500c4bcb66c1a1fa47d67250bb35de4f
maxhaesslein <git@maxhaesslein.de>
2025-03-14 11:58:50 +0100

    automatically create data folder if it is missing

--- include/variables.php
+++ include/variables.php
@@ -8,6 +8,17 @@ $version = '0.3';
 $group = $_REQUEST['group'] ?? false;
 
 
+// automatically create data folder, if it is missing
+if( ! is_dir('data') ) {
+	$oldumask = umask(0); // we need this for permissions of mkdir to be set correctly
+	if( mkdir( 'data/', 0775, false ) === false ) {
+		echo '<p><strong>Error: could not create <em>data/</em> subfolder.</strong> Please make sure that the folder is writeable.</p>';
+		exit;
+	}
+	umask($oldumask); // we need this after changing permissions with mkdir
+}
+
+
 $topics = [];
 
 if( $group ) {
